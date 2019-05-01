9a44dcbed2c645624037d3906e30046ce39ea075
Elizabeth <elizabethallen36@gmail.com>
2019-04-30 20:25:52 -0700

    fix??? working with time stuff

--- built-in-objects/js/scripts.js
+++ built-in-objects/js/scripts.js
@@ -3,14 +3,42 @@ var date = new Date(); //new date with vuttent date and time
     console.log(date);
     
 
-var newDate = new Date ("Januaray 31, 2019");
-console.log(newDate);
+
 
 console.log(date.getFullYear());
-console.log(date.getFullYear()); //js starts at 1900 for some god forsaken reason
+console.log(date.getYear()); //js starts at 1900 for some god forsaken reason
 
 console.log(date.getDate());
 console.log(date.getMonth());
 console.log(date.getMinutes());
 console.log(date.getHours());
+console.log(date.getTime());
 
+var month = date.getUTCMonth();
+var day = date.getDate();
+var year = date.getFullYear(); //() ALWAYS NEEDED AFTER THE GETTIME COMMAND
+
+if (month.length < 10  ){
+    month = '0'+ month;
+ }
+var formattedDate = month + '/' + day + '/' + year;
+console.log(formattedDate);
+
+var newDate = new Date ("Januaray 31, 2019");
+
+// set hour =18
+newDate.setHours(18);
+// s3et minute = 32
+newDate.setMinutes(32);
+// set second =45
+newDate.setSeconds(45);
+console.log(newDate);
+
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+
+// set timeout
+setTimeout(function(){
+    console.log(new Date());
+    
+}, 5000);
