a187ed50ebe010862a4c83e0a3aaffe7531c3fd6
Elizabeth <elizabethallen36@gmail.com>
2019-04-30 20:00:28 -0700

    date.gettime

---
+++ built-in-objects/js/scripts.js
@@ -0,0 +1,16 @@
+// date object
+var date = new Date(); //new date with vuttent date and time
+    console.log(date);
+    
+
+var newDate = new Date ("Januaray 31, 2019");
+console.log(newDate);
+
+console.log(date.getFullYear());
+console.log(date.getFullYear()); //js starts at 1900 for some god forsaken reason
+
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getMinutes());
+console.log(date.getHours());
+
