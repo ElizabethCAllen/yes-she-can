29128ef3bcf6b027a6a04d84c5e5e9a77262222a
Elizabeth <elizabethallen36@gmail.com>
2019-04-30 20:46:32 -0700

    beep

---
+++ built-in-objects/js/math.js
@@ -0,0 +1,15 @@
+// math objects
+var random = Math.random() * 1000;
+console.log(random);
+
+console.log('round', Math.round(random));//rounds up to nearest integer
+console.log('floor', Math.floor(random)); // fources down to nears integer
+console.log('ceil', Math.ceil(random)); // forces up to nears integer
+
+var x = 10,
+    y = 20;
+console.log('max', Math.max(x,y));
+console.log('min', Math.min(x,y));
+
+var z = 300;
+console.log('absolute', Math.abs(z)); //return absolute balue of the variable
